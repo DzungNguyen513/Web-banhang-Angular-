2cba1d78dd69f8e8636a5cb2e7fe07603c15d628
DzungNguyen513 <nguyentrungdung0501@gmail.com>
2024-05-10 11:14:58 +0700

    design HonePage

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
-    AppModule,
+    AppModule, // Thêm AppModule vào đây
     ServerModule,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [HomeComponent],
 })
 export class AppServerModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [
     provideClientHydration()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
